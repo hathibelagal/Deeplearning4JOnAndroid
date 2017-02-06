b64abbf6b796fc10a13895a876eb1f25c0003c20
Hathibelagal <mlrdae@gmail.com>
2017-02-06 16:19:00 +0530

    Initial commit

---
+++ app/src/main/java/com/progur/deeplearningtutorial/MainActivity.java
@@ -0,0 +1,135 @@
+package com.progur.deeplearningtutorial;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.reflections.vfs.CommonsVfs2UrlType;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.R.id.input;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                useNetwork();
+            }
+        });
+
+    }
+
+    private void useNetwork() {
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.my_network);
+            MultiLayerNetwork myNetwork =
+                    ModelSerializer.restoreMultiLayerNetwork(inputStream);
+
+            // Create input
+            INDArray actualInput = Nd4j.zeros(1,2);
+            actualInput.putScalar(new int[]{0,0}, 1);
+            actualInput.putScalar(new int[]{0,1}, 0);
+
+            // Generate output
+            INDArray actualOutput = myNetwork.output(actualInput);
+            Log.d("myNetwork Output", actualOutput.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createAndUseNetwork() {
+        DenseLayer inputLayer = new DenseLayer.Builder()
+                .nIn(2)
+                .nOut(3)
+                .name("Input")
+                .build();
+
+        DenseLayer hiddenLayer = new DenseLayer.Builder()
+                .nIn(3)
+                .nOut(2)
+                .name("Hidden")
+                .build();
+
+        OutputLayer outputLayer = new OutputLayer.Builder()
+                .nIn(2)
+                .nOut(2)
+                .name("Output")
+                .activation(Activation.SOFTMAX)
+                .build();
+
+        NeuralNetConfiguration.Builder nncBuilder = new NeuralNetConfiguration.Builder();
+        nncBuilder.iterations(60000);
+        nncBuilder.learningRate(0.1);
+
+        NeuralNetConfiguration.ListBuilder listBuilder = nncBuilder.list();
+        listBuilder.layer(0, inputLayer);
+        listBuilder.layer(1, hiddenLayer);
+        listBuilder.layer(2, outputLayer);
+
+        listBuilder.backprop(true);
+
+        MultiLayerNetwork myNetwork = new MultiLayerNetwork(listBuilder.build());
+        myNetwork.init();
+
+        final int NUM_SAMPLES = 4;
+
+        INDArray trainingInputs = Nd4j.zeros(NUM_SAMPLES, inputLayer.getNIn());
+        INDArray trainingOutputs = Nd4j.zeros(NUM_SAMPLES, outputLayer.getNOut());
+
+        // If 0,0 show 0
+        trainingInputs.putScalar(new int[]{0,0}, 0);
+        trainingInputs.putScalar(new int[]{0,1}, 0);
+        trainingOutputs.putScalar(new int[]{0,0}, 0);
+        trainingOutputs.putScalar(new int[]{0,1}, 1);
+
+        // If 0,1 show 1
+        trainingInputs.putScalar(new int[]{1,0}, 0);
+        trainingInputs.putScalar(new int[]{1,1}, 1);
+        trainingOutputs.putScalar(new int[]{1,0}, 1);
+        trainingOutputs.putScalar(new int[]{1,1}, 0);
+
+        // If 1,0 show 1
+        trainingInputs.putScalar(new int[]{2,0}, 1);
+        trainingInputs.putScalar(new int[]{2,1}, 0);
+        trainingOutputs.putScalar(new int[]{2,0}, 1);
+        trainingOutputs.putScalar(new int[]{2,1}, 0);
+
+        // If 1,1 show 0
+        trainingInputs.putScalar(new int[]{3,0}, 1);
+        trainingInputs.putScalar(new int[]{3,1}, 1);
+        trainingOutputs.putScalar(new int[]{3,0}, 0);
+        trainingOutputs.putScalar(new int[]{3,1}, 1);
+
+        DataSet myData = new DataSet(trainingInputs, trainingOutputs);
+        myNetwork.fit(myData);
+
+        // Create input
+        INDArray actualInput = Nd4j.zeros(1,2);
+        actualInput.putScalar(new int[]{0,0}, 1);
+        actualInput.putScalar(new int[]{0,1}, 1);
+
+        // Generate output
+        INDArray actualOutput = myNetwork.output(actualInput);
+        Log.d("myNetwork Output", actualOutput.toString());
+    }
+}
